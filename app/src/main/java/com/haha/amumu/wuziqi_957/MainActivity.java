9b99873bc5a693b6ffaa8b53be1f344205f56fad
muyoumumumu <muyoumumumu@hotmail.com>
2016-10-26 19:34:59 +0800

    第二次

--- app/src/main/java/com/haha/amumu/wuziqi_957/MainActivity.java
+++ app/src/main/java/com/haha/amumu/wuziqi_957/MainActivity.java
@@ -2,12 +2,23 @@ package com.haha.amumu.wuziqi_957;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.baidu.com/");
     }
 }
